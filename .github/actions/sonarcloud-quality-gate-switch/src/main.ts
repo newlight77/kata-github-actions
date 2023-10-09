3e608909c881d7026052959ab9038046e588fd0b
Kong To <newlight77@gmail.com>
2023-10-09 15:53:58 +0200

    implementing sonarcloud-quality-gate-switch action

---
+++ .github/actions/sonarcloud-quality-gate-switch/src/main.ts
@@ -0,0 +1,12 @@
+import * as core from '@actions/core';
+import { selectGate } from './selectGate';
+
+async function run() {
+    const sonarToken: string = core.getInput('sonarcloudToken');
+    const projectKey: string = core.getInput('projectKey');
+    const gateName: string = core.getInput('tmpGateName');
+
+    selectGate(sonarToken, projectKey, gateName);
+}
+
+run();
